c3691e2ebc45546a687739c71893c0d437207c29
Kriska939 <cristinacorrado93@gmail.com>
2021-10-01 16:18:56 +0200

    adding header/general css

---
+++ resources/views/layouts/structure.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@400;500;700&display=swap" rel="stylesheet">
+    <title>Laravel Comics</title>
+</head>
+<body>
+@include('includes.header')
+    <main>
+   <!-- INSERISCI QUI YIELD PER IL CONTENUTO FUMETTI --> 
+    </main>
+ @include('includes.footer')
+</body>
+</html>
