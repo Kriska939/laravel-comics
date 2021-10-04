57d5ace039caa7c73c10d6bc20220f60fb6b2a8a
Kriska939 <cristinacorrado93@gmail.com>
2021-10-04 15:06:12 +0200

    added footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,53 @@
+<footer>
+    <div id="top-footer" class="flex">
+      <section id="columns" class="container">
+        <ul>
+          <h4>DC COMICS</h4>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Tv</a></li>
+        </ul>
+        <ul>
+          <h4>DC COMICS</h4>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Tv</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Video</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+        <ul>
+          <h4>DC COMICS</h4>
+          <li><a href="#">Test</a></li>
+          <li><a href="#">Test</a></li>
+          <li><a href="#">Test</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+        <ul>
+          <h4>DC COMICS</h4>
+          <li><a href="#">Test</a></li>
+          <li><a href="#">Test</a></li>
+          <li><a href="#">Test</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+      </section>
+      <section id="DCHalf">
+        <img src="{{ asset('img/dc-logo-bg.png') }}" alt="DC big logo" />
+      </section>
+    </div>
+    <section id="social">
+      <div class="container flex j-between">
+        <button>SIGN-UP NOW!</button>
+        <div class="flex" id="followContainer">
+          <h3>Follow Us</h3>
+          <img src="../img/footer-facebook.png" alt="Facebook Logo" />
+          <img src="../img/footer-twitter.png" alt="Twitter Logo" />
+          <img src="../img/footer-youtube.png" alt="Youtube Logo" />
+          <img src="../img/footer-pinterest.png" alt="Pinterest Logo" />
+          <img src="../img/footer-periscope.png" alt="Periscope Logo" />
+        </div>
+      </div>
+    </section>
+  </footer>
