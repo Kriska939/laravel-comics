a7f817667748f9709ec60663910a829006479cb1
Kriska939 <cristinacorrado93@gmail.com>
2021-10-04 23:03:58 +0200

    added dynamic comic page

--- resources/views/comicdetail.blade.php
+++ resources/views/comicdetail.blade.php
@@ -7,19 +7,22 @@
 <div class="small-container">
         <div class="row">
             <div class="col">
-                <h1>TITOLO PLACEHOLDER</h1>
+                <h1>{{$comic['title']}}</h1>
                 <div class="flex w-600 j-between bg-green">
-                    <div class="price-info flex">
-                            <p>U.S. Price: </p>
-                            <p>AVAILABLE</p>
+                    <div class="price-info flex j-between">
+                            <p>U.S. Price: <span>{{$comic['price']}}</span> </p>
+                            <p>Available</p>
                     </div>
                     <div class="availability-info">
                         <p>Check availability <i class="fas fa-sort-down"></i></p>
                     </div>
                 </div>
-                <div class="description">
-                    <p>descrizione specifica del fumetto</p>
+                <div class="description w-600 ">
+                    <p>{{$comic['description']}}</p>
                 </div>
+            </div>
+            <div class="col w-300">
+                <img src="{{ asset('img/adv.jpg')}}" alt="spam">
             </div>
         </div>
 </div>
